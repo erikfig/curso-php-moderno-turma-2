8ae6de28724728798b355a3283572794f8f93380
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-08-29 12:26:07 -0300

    Primeiros testes

--- exemplos/querybuilder/src/Query.php
+++ exemplos/querybuilder/src/Query.php
@@ -6,6 +6,9 @@ class Query
 {
 	protected $table;
 	private $select_str = 'SELECT * FROM %s;';
+	private $delete_str = 'DELETE FROM %s;';
+	private $update_str = 'UPDATE %s SET %s;';
+	private $insert_str = 'INSERT INTO %s (%s) VALUES (%s);';
 
 	public function table($table)
 	{
@@ -16,5 +19,25 @@ class Query
 	public function select()
 	{
 		return sprintf($this->select_str, $this->table);
+	}
+
+	public function delete()
+	{
+		return sprintf($this->delete_str, $this->table);
+	}
+
+	public function update(Array $data)
+	{
+		$str = '';
+		foreach ($data as $k=>$v)
+			$str = '`'.$k.'`=:'.$k;
+		return sprintf($this->update_str, $this->table, $str);
+	}
+
+	public function insert($data)
+	{
+		$fields = implode('`, `', array_keys($data));
+		$values = implode(', :', array_keys($data));
+		return sprintf($this->insert_str, $this->table, '`'.$fields.'`', ':'.$values);
 	}
 }
