cca5be3d0f944b6147bd723e498f79bee4d38f72
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-08-29 11:50:02 -0300

    Testes

--- exemplos/querybuilder/tests/QueryTest.php
+++ exemplos/querybuilder/tests/QueryTest.php
@@ -14,4 +14,32 @@ class QueryTest extends PHPUnit_Framework_TestCase
 
 		$this->assertEquals('SELECT * FROM users;', $sql);
 	}
+
+	public function testSimpleUpdate()
+	{
+		$query = new Query;
+		$sql = $query->table('users')
+			->update(['name'=>'Erik']);
+
+		$this->assertEquals('UPDATE users SET `name`=:name;', $sql);
+	}
+
+	public function testSimpleInsert()
+	{
+		$query = new Query;
+		$sql = $query->table('users')
+			->insert(['name'=>'erik', 'lastname'=>'figueiredo']);
+
+		$this->assertEquals('INSERT INTO users (`name`, `lastname`) VALUES (:name, :lastname);', $sql);
+	}
+
+	public function testSimpleDelete()
+	{
+		$query = new Query;
+		$sql = $query->table('users')
+			->delete();
+
+		$this->assertEquals('DELETE FROM users;', $sql);
+	}
+
 }
