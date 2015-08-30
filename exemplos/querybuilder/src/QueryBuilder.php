d8f85488652fd8af1d5527a2c1bc698db3348029
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-08-30 10:18:28 -0300

    Começo segundo dia

---
+++ exemplos/querybuilder/src/QueryBuilder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PhpModerno\Query;
+
+class QueryBuilder
+{
+	private static $db;
+	private static $is_connected;
+	private static $sql;
+
+	public static function getDb(Array $config)
+	{
+		if (is_null(self::$db))
+			self::$db = new Db;
+
+		self::$db->config($config);
+
+		return self::$db;
+	}
+
+	public static function query($table)
+	{
+		self::$sql = new Query();
+		self::$sql->table($table);
+		return self::$sql;
+	}
+
+	public static function getOne()
+	{
+		self::conn();
+		return self::$db->getOne(self::$sql);
+	}
+
+	public static function getAll()
+	{
+		self::conn();
+		return self::$db->getOne(self::$sql);
+	}
+
+	protected static function conn()
+	{
+		if (!self::$is_connected) {
+			self::$db->conn();
+			self::$is_connected = true;
+		}
+	}
+
+}
